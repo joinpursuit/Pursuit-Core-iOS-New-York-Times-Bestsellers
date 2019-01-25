25f777ec7fd07100a4bf039510569916b8986a2f
SLRAM <stephier2290@gmail.com>
2019-01-25 17:31:14 -0500

    Favorites Cell formatted

--- NYTBestsellers/NYTBestsellers/Cells/FavoritesCollectionViewCell.swift
+++ NYTBestsellers/NYTBestsellers/Cells/FavoritesCollectionViewCell.swift
@@ -9,12 +9,91 @@
 import UIKit
 
 class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    public lazy var cellImage: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "Placeholder"))
+        return iv
+    }()
+    
+    public lazy var cellLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .black
+        label.text = "Label"
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    public lazy var cellTextView: UITextView = {
+        let tv = UITextView()
+        tv.text = "Description..."
+        return tv
+    }()
+    
+    lazy var cellButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("...", for: .normal)
+        button.addTarget(self, action: #selector(cellButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    @objc func cellButtonPressed() {
+//        guard let message = mainTextField.text else {return}
+//        delegate?.enterButtonPressed(message: message)
+        
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .red
+        commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupCells()
+    }
+}
+extension FavoritesCollectionViewCell {
+    private func setupCells() {
+        setupCellImageView()
+        setupCellLabel()
+        setupCellTextView()
+        setupCellButton()
+    }
+    
+    private func setupCellImageView () {
+        addSubview(cellImage)
+        cellImage.translatesAutoresizingMaskIntoConstraints = false
+        cellImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 11).isActive = true
+        cellImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 90).isActive = true
+        cellImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -90).isActive = true
+        cellImage.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.50).isActive = true
+    }
+    
+    private func setupCellLabel () {
+        addSubview(cellLabel)
+        cellLabel.translatesAutoresizingMaskIntoConstraints = false
+        cellLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 11).isActive = true
+        cellLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11).isActive = true
+        cellLabel.topAnchor.constraint(equalTo: cellImage.bottomAnchor, constant: 11).isActive = true
+        
+    }
+    private func setupCellTextView () {
+        addSubview(cellTextView)
+        cellTextView.translatesAutoresizingMaskIntoConstraints = false
+        cellTextView.topAnchor.constraint(equalTo: cellLabel.bottomAnchor, constant: 11).isActive = true
+        cellTextView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -11).isActive = true
+        cellTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11).isActive = true
+        cellTextView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11).isActive = true
+    }
+    private func setupCellButton () {
+        addSubview(cellButton)
+        cellButton.translatesAutoresizingMaskIntoConstraints = false
+        cellButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        cellButton.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
     }
 }
