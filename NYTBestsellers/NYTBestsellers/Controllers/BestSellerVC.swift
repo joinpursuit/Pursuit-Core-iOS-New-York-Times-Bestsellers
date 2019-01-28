b2f838b513d723bbfeb365e9630d6e76bf83edb8
Joshua Viera <mymacbook@Joshuas-MacBook-Pro.local>
2019-01-28 10:41:13 -0500

    Started Fresh Project(git issues), Initial Commit

---
+++ NYTBestsellers/NYTBestsellers/Controllers/BestSellerVC.swift
@@ -0,0 +1,30 @@
+//
+//  BestSellerVC.swift
+//  NYTBestsellers
+//
+//  Created by Joshua Viera on 1/28/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class BestSellerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NYTBestsellers/NYTBestsellers/Views/OptionsView.swift
@@ -0,0 +1,21 @@
+//
+//  OptionsView.swift
+//  NYTBestsellers
+//
+//  Created by Joshua Viera on 1/28/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class OptionsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
