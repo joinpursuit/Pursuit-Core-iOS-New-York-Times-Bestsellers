5d1eeb9f6be1c1d23c714a51081afd0b2a75b161
Jian Ting Li <jiantingli@Jian-Tings-MacBook-Pro.local>
2019-01-28 01:59:46 -0500

    setting up Collection View of Bestsellers

--- NYTBestsellers/NYTBestsellers/View/NYTBestSellingView.swift
+++ NYTBestsellers/NYTBestsellers/View/NYTBestSellingView.swift
@@ -9,13 +9,58 @@
 import UIKit
 
 class NYTBestSellingView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    lazy var bestsellersCollectionView: UICollectionView = {
+       let cv = UICollectionView()
+        cv.dataSource = self
+        cv.delegate = self
+        cv.backgroundColor = .yellow
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .blue
+        setupViews()
+    }
+    
 
+}
+
+extension NYTBestSellingView {
+    private func setupViews() {
+        setupBestSellingCollectionView()
+        setupCategoryPicker()
+    }
+    
+    private func setupBestSellingCollectionView() {
+        
+    }
+    
+    private func setupCategoryPicker() {
+        
+    }
+}
+
+extension NYTBestSellingView: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = bestsellersCollectionView.dequeueReusableCell(withReuseIdentifier: "BookCell", for: indexPath) as? BookCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: 250/1.5, height: 350/1.5)
+    }
 }
