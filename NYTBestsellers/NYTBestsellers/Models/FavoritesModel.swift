f0e7fbe58b853610127241425d19464fa53f2e63
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-01-29 15:27:21 -0500

    wrote manager helper and model for Favorites. Realized i NEED A BUTTON TO favorite in the detail, next item on list

--- NYTBestsellers/NYTBestsellers/Models/FavoritesModel.swift
+++ NYTBestsellers/NYTBestsellers/Models/FavoritesModel.swift
@@ -8,4 +8,29 @@
 
 import Foundation
 
-
+struct FavoriteBooks: Codable {
+  let imageData: Data
+  let weeksOnBestSellerList: String
+  let bookDescription: String
+  let createdAt: String
+  
+  public var dateFormattedString: String {
+    let isoDateFormatter = ISO8601DateFormatter()
+    var formattedDate = createdAt
+    if let date = isoDateFormatter.date(from: createdAt) {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "MMMM d, yyyy hh:mm a"
+      formattedDate = dateFormatter.string(from: date)
+    }
+    return formattedDate
+  }
+  
+  public var date: Date {
+    let isoDateFormatter = ISO8601DateFormatter()
+    var formattedDateFromDate = Date()
+    if let date = isoDateFormatter.date(from: createdAt){
+      formattedDateFromDate = date
+    }
+    return formattedDateFromDate
+  }
+}
